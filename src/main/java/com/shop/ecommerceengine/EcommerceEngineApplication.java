8fc617f5c72527ed04e6d50943148a435442087e
kaytee07 <kofi.taylor07@gmail.com>
2026-01-08 09:21:27 +0000

    AUTHENTICATION AND AUTHORIZATION FOR E-COMMERCE ENGINE

---
+++ src/main/java/com/shop/ecommerceengine/EcommerceEngineApplication.java
@@ -0,0 +1,13 @@
+package com.shop.ecommerceengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceEngineApplication.class, args);
+    }
+
+}
